3808b98bf68b959bad73a7428e00ee5548a4ad21
jako6857 <82064889+jako6857@users.noreply.github.com>
2025-03-18 12:29:50 +0100

    opg 1

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,93 +1,57 @@
-
-
 /*opgave 1 plus minus*/
 let myButtonValue = 0;
 //feltet der vises resultat i
 
-const myButtonResult = document.getElementById('buttonResult');
-const myAddButton = document.getElementById('addButton');
-const mySubButton = document.getElementById('subtractButton');
-
+const myButtonResult = document.getElementById("buttonResult");
+const myAddButton = document.getElementById("addButton");
+const mySubButton = document.getElementById("subtractButton");
 
 // skriv din eventhandler kode her ---------------------------------------
 
-
-
+myAddButton.addEventListener("click", addValue);
+mySubButton.addEventListener("click", subtractValue);
 
 //-------------------------------------------------------------------------
 
 // named functions du kan kalde fra eventhandlers
 function subtractValue() {
- 
-    myButtonValue = myButtonValue - 1;
+  myButtonValue = myButtonValue - 1;
 
-    showResult('værdi: ' + myButtonValue, myButtonResult)
+  showResult("værdi: " + myButtonValue, myButtonResult);
 }
-
 
 function addValue() {
- 
-    myButtonValue = myButtonValue + 1;
+  myButtonValue = myButtonValue + 1;
 
-    showResult('værdi: ' + myButtonValue, myButtonResult)
+  showResult("værdi: " + myButtonValue, myButtonResult);
 }
 
-
-
-
-
-
 /* opgave 2 dice*/
-const myDiceRes = document.getElementById('diceResult');
-const diceButton = document.getElementById('rollDiceOne');
+const myDiceRes = document.getElementById("diceResult");
+const diceButton = document.getElementById("rollDiceOne");
 
-
-
-  
-    /* disse to liner skal afvikles i en arrow funktion bundet til en eventhandler på diceButton
+/* disse to liner skal afvikles i en arrow funktion bundet til en eventhandler på diceButton
     let diceRoll = getRandomNumber(1, 6);
     showResult(diceRoll, myDiceRes) */
 
 // skriv din eventhandler kode her ---------------------------------------
 
-
-
-
 //-------------------------------------------------------------------------
-
-
-
-
-
-
-
-
 
 /* opgave 3 key event */
 
-const myLiveText = document.getElementById('myLiveText');
-let myLiveTextResult = document.getElementById('tasteResult');
+const myLiveText = document.getElementById("myLiveText");
+let myLiveTextResult = document.getElementById("tasteResult");
 
 // skriv din eventhandler kode her ---------------------------------------
 
-
-
-
 //-------------------------------------------------------------------------
-
-
-
-
-
-
-
 
 /* view code  DO NOT FIDDLE WIT THIS CODE*/
 /* viser data i et DOM element, kræver to parametre: data til at vise og elementet det skal vises i.*/
 function showResult(myData, myElement) {
-    myElement.innerHTML = myData;
+  myElement.innerHTML = myData;
 }
-
 
 /* support functions-------------------------------------------------------------------------------*/
 
@@ -95,8 +59,7 @@ function showResult(myData, myElement) {
 returnerer et tilfældigt tal mellem min og max værdierne*/
 
 function getRandomNumber(min, max) {
-    let myMin = Math.ceil(min);
-    let myMax = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+  let myMin = Math.ceil(min);
+  let myMax = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
 }
-
